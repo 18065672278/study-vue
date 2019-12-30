ace416aed717c10fe12cb3e6a9e4f7186a6e31d7
hujl <845769627@qq.com>
2019-12-30 17:19:10 +0800

    2019.12.30-1 Tj

--- week2-day3/src/main.js
+++ week2-day3/src/main.js
@@ -1,2 +1,19 @@
-let a = require('./a.js');
-console.log(a);
+let str = require('./a.js');
+console.log(str);
+let a = b => c => d => b + c + d;
+console.log(a(2)(3)(4));
+
+let obj = {name:'aaa'};
+let obj1 = {age:8};
+let newObj = {...obj,...obj1};//es7
+console.log(newObj);
+
+import './index.css';//引入css文件
+import './style.less';
+
+//在js中引入图片需要import，或者直接写一个线上路径
+import page from './1.png';
+console.log(page);//page 就是打包后图片的路径
+let img = new Image();
+img.src = page;//写一个字符串，webpack不会进行查找
+document.body.appendChild(img);
--- week2-day3/webpack.config.js
+++ week2-day3/webpack.config.js
@@ -1,10 +1,37 @@
 //webpack 必须采用common的写法
 let path = require('path');//专门处理路径用的
 console.log(path.resolve('.dist'));//已当前路径解析一个相对路径
+let HtmlWebpagePlugin = require('html-webpack-plugin');
 module.exports = {
-    entry:'./src/main',//打包的入口文件，webpack会自动查找相关依赖的包进行打包
-    output:{
-        filename:'builder.js',//打包后的名字（自己起的）
+    entry: {main: './src/main.js'},//打包的入口文件，webpack会自动查找相关依赖的包进行打包
+    output: {
+        filename: 'build.js',//打包后的名字（自己起的）
         path: path.resolve('.dist')//必须是一个绝对路径
-    }
+    },
+    //模块解析规则
+    //-js 匹配所有的js 用babel-loader转译 排除掉node-modules
+    module: {
+        rules: [
+            {
+                test: /\.js$/, use: 'babel-loader', exclude: /node-modules/
+            },
+            //use时从右往左写
+            {
+                test: /\.css$/, use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/, use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            {
+                //转换base64只在8K（8192）以下转化，其他的直接输出图片
+                test: /\.(jpg|png|gif)$/, use: 'url-loader?limit=8192'
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpagePlugin({//自动插入到dist目录中
+            template:'./src/index.html',//使用的模板
+            //filename: 'login.html'//产出的文件名字（默认为index.html）
+        })
+    ],
 }
